197095a0bc466eeee0e874a0d6d650aa4d7372b2
luffyu-dev <rockyu@lexin.com>
2022-11-29 10:23:19 +0800

    去掉不用的sal

--- rubber-at-tennis-atp-manager/src/main/java/com/rubber/at/tennis/atp/manager/sal/DemoSal.java
+++ rubber-at-tennis-atp-manager/src/main/java/com/rubber/at/tennis/atp/manager/sal/DemoSal.java
@@ -1,14 +1,10 @@
 package com.rubber.at.tennis.atp.manager.sal;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
 
 /**
  * @author luffyu
  * Created on 2022/8/13
  */
-@Slf4j
-@Component
 public class DemoSal {
 
 }
