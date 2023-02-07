2dab317111197770185962f669e671ce6a49fc9e
luffyu-dev <rockyu@lexin.com>
2023-02-07 22:05:22 +0800

    1、新增player的数据优化 2、新增赛事信息的数据

--- rubber-at-tennis-atp-web/src/main/java/com/rubber/at/tennis/atp/web/RubberAtTennisAtpWebApplication.java
+++ rubber-at-tennis-atp-web/src/main/java/com/rubber/at/tennis/atp/web/RubberAtTennisAtpWebApplication.java
@@ -5,12 +5,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import java.util.TimeZone;
+
 @ComponentScan("com.rubber.*")
 @MapperScan("com.rubber.at.tennis.**.dao.mapper")
 @SpringBootApplication
 public class RubberAtTennisAtpWebApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("GMT+8"));
         SpringApplication.run(RubberAtTennisAtpWebApplication.class, args);
     }
 
